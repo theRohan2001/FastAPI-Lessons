b8e16a9deead9737535f71e3d8c290402ae1a9ad
theRohan2001 <rohanwadhwa2001@gmail.com>
2025-08-29 15:56:10 +0530

    lesson-1

--- Lesson_1/books.py
+++ Lesson_1/books.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
+from models.book import Book, BookResponse
 
 router = APIRouter()
 
-
+#path paratemer
 @router.get("/books/{book_id}")
 async def read_book(book_id: int):
     return{
@@ -11,6 +12,7 @@ async def read_book(book_id: int):
         "author": "J.R.R. Tolkien"
     }
 
+#Query parameter
 @router.get("/books")
 async def read_books(year: int | None = None):
     if year:
@@ -19,3 +21,26 @@ async def read_books(year: int | None = None):
             "books": {"book1", "book2"}
         }
     return {"books": ["All Books"]}
+
+# create a book using pydantic model
+@router.post("/book")
+async def create_book(book: Book):
+    return book
+
+
+@router.get("/allbooks", response_model=list[BookResponse])
+async def read_all_books():
+    return[
+            {
+                "title" : "1984", 
+                "author" : "George Orwell",
+                "year" : 1923
+
+            },
+            {
+                "title" : "LOR", 
+                "author" : "J.R.R. Tolkien", 
+                "year" : 1923
+                
+            }
+        ]
--- Lesson_1/main.py
+++ Lesson_1/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+import json
+from fastapi import FastAPI, HTTPException, status, Request
+from fastapi.responses import JSONResponse, PlainTextResponse
+from fastapi.exceptions import RequestValidationError
 import books
 import author
 
@@ -7,6 +10,32 @@ app = FastAPI()
 app.include_router(books.router)
 app.include_router(author.router)
 
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    return JSONResponse(
+        status_code = exc.status_code, 
+        content={
+            "message": "Oops! Something went wrong"
+        }
+    )
 
 
 
+
+@app.get("/error_endpoint")
+async def raise_exception():
+    raise HTTPException(status_code= status.HTTP_400_BAD_REQUEST)
+
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+    request: Request,
+    exc: RequestValidationError
+):
+    return PlainTextResponse(
+        "This is plain text response"
+        f" \n{json.dumps(exc.errors(), indent=2)}",
+        status_code = status.HTTP_400_BAD_REQUEST
+        
+        )
